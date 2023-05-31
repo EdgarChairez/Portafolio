72654d5db95ac4d3f58bbcf2c47700d1dce89035
Edgar Chairez <chairezandres0@gmail.com>
2023-05-31 14:57:03 -0700

    Se agregan mejoras

--- js/main.js
+++ js/main.js
@@ -5,7 +5,30 @@ const  typewriter = new Typewriter(app, {
     delay:75
 });
 
-typewriter
-    .typeString('Proximamente - Portafolio de Proyectos')
+// var ancho = window.innerWidth;
+var ancho = outerWidth;
+
+function validarTamanoPantalla() {
+    var mensaje = "";
+
+    if (ancho < 1000) {
+        mensaje = "Proximamente - Portafolio";
+    } else {
+        mensaje = "Proximamente - Portafolio de Proyectos";
+    }
+
+    typewriter
+    .typeString(mensaje)
     .pauseFor(200)
     .start()
+}
+
+// Llamada a la función cuando se carga la página
+window.onload = function() {
+    validarTamanoPantalla();
+};
+  
+// Llamada a la función cuando se redimensiona la ventana
+// window.onresize = function() {
+//     validarTamanoPantalla();
+// };
